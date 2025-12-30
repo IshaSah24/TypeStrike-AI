6194cea9443966bd359bb8715f30ed8c58960b35
Isha Sah <131801898+IshaSah24@users.noreply.github.com>
2025-12-30 17:28:05 +0530

    updated url for  frontend in production

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -13,13 +13,14 @@ import { v4 as uuidv4 } from "uuid";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 const app = express();
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: FRONTEND_URL,
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -27,7 +28,7 @@ const io = new Server(server, {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: FRONTEND_URL,
     credentials: true,
   })
 );
